a37d0557b21799f85bea04419212358703dcecc4
Andrew Johnson <blackandred@users.noreply.github.com>
2022-04-03 19:56:55 +0200

    tests: Increase coverage

--- generate/templating_test.go
+++ generate/templating_test.go
@@ -37,3 +37,30 @@ func TestRenderTemplate_WithValidParams(t *testing.T) {
     assert.Contains(t, out, "exec backup-maker make") // the last command is backup-maker (may change within the template, just like line above)
     assert.Nil(t, err)
 }
+
+func TestRenderTemplate_FailsWhenAnyVariableIsMissing(t *testing.T) {
+    params := map[string]string{
+        "hostname": "postgres.db.svc.cluster.local",
+        "port":     "5432",
+        // NOTICE: a few variables there are missing: db, user, password
+    }
+
+    repository := map[string]string{
+        "url":               "https://example.org",
+        "token":             "${BR_TOKEN}",
+        "encryptionKeyPath": "/var/lib/backup-repository/encryption.key",
+        "passphrase":        "${GPG_PASSPHRASE}",
+        "recipient":         "your-gpg@email.org",
+        "collectionId":      "111-222-333-444",
+    }
+
+    template := generate.Templating{}
+    _, err := template.RenderTemplate("postgres", "backup", map[string]interface{}{
+        "Params":     params,
+        "Repository": repository,
+    })
+
+    assert.NotNil(t, err)
+    assert.Contains(t, err.Error(), "templates/backup/postgres.tmpl")
+    assert.Contains(t, err.Error(), "map has no entry for key")
+}
